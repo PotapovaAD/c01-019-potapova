875cc111bc609cbbbd1dbe548f4fc4ef37f6edf4
PotapovaAD <74531925+PotapovaAD@users.noreply.github.com>
2020-11-16 20:36:46 +0300

    1.py
    Add program

--- 1.py
+++ 1.py
@@ -1,1 +1,16 @@
-
+learners_french = input().split()
+pianists = input().split()
+swimmers = input().split()
+X = Y = Z = set()
+for a in learners_french:
+	X.add(a)
+for b in pianists:
+	Y.add(b)
+for c in swimmers:
+	Z.add(c)
+L = set()
+L = (Y.intersection(Z)).difference(X)
+result = []
+for x in D:
+	result.append(x)
+print(*result)
